5ee2e021085c227e40f6f2781f506d5e65dac112
lukehayes <lukepurplehayes@gmail.com>
2021-07-26 09:11:04 +0100

    Added core/block module to project.

---
+++ core/block/block.py
@@ -0,0 +1,9 @@
+
+class Block:
+    """Base class for a block."""
+    def __init__(self):
+        self.speed = 5
+        self.r = 255
+        self.g = 0
+        self.b = 255
+
---
+++ core/block/sand_block.py
@@ -0,0 +1,9 @@
+from core.block import block
+
+class SandBlock(block.Block):
+    """docstring for SandBlock"""
+    def __init__(self):
+        super(SandBlock, self).__init__()
+
+
+
