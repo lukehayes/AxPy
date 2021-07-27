90ed4e2fbe92e216d181da20acbe9b0d7918512f
lukehayes <lukepurplehayes@gmail.com>
2021-07-27 21:27:39 +0100

    Added default width and height to block base class.

--- core/block/block.py
+++ core/block/block.py
@@ -3,6 +3,8 @@ class Block:
     """Base class for a block."""
     def __init__(self):
         self.speed = 5
+        self.width = 10
+        self.height = 10
         self.r = 255
         self.g = 0
         self.b = 255
