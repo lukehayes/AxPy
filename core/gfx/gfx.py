df0df3975273aa2fb63fd8bd09c6143461731f60
lukehayes <lukepurplehayes@gmail.com>
2021-07-29 17:22:16 +0100

    Added Gfx.drawRect() function.

--- core/gfx/gfx.py
+++ core/gfx/gfx.py
@@ -3,3 +3,7 @@ import pygame
 def draw(screen, x, y, w, h):
     """Wrapper for pygame drawing"""
     pygame.draw.rect(screen, pygame.Color(255,0,255), pygame.Rect(x,y,w,h))
+
+def drawRect(screen, rect):
+    """Draw a pygame.Rect abstraction"""
+    pygame.draw.rect(screen, pygame.Color(255,0,255), rect)
