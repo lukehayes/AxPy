f91f369188e647866c8ec70a9e0e2eaa7c75198d
lukehayes <lukepurplehayes@gmail.com>
2021-08-01 19:20:53 +0100

    Main.py now holds updated particle emitter code.

--- main.py
+++ main.py
@@ -4,24 +4,36 @@ from core.block import Block
 from core.block import Grid
 from core.gfx import draw,drawBlock
 from core.engine import Engine
+from core.particles import ParticleEmitter
+from core.particles import Particle
+import random
 
 SPACE = 6
-engine = Engine(800,600)
+engine = Engine(1024, 768)
 
-screen = pygame.display.set_mode((engine.width, engine.height))
+screen = pygame.display.set_mode((engine.width, engine.height), pygame.RESIZABLE)
 pygame.display.set_caption('Physics')
 screen.fill(engine.bg_color)
 pygame.display.flip()
 running = True
 
+
 c = 0
 
 b = Block(10,10)
-gridObject= Grid(10,10)
-grid = gridObject.create()
-
 dt = 0
 clock  = pygame.time.Clock()
+
+emitters = []
+particles = ParticleEmitter(150,650)
+
+for x in range(1,10):
+    e = ParticleEmitter(
+        random.randint(10,1000),
+        random.randint(10,700)
+    )
+
+emitters.append(e)
 
 while running:
     c = c + 0.01
@@ -29,14 +41,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    print(dt)
     screen.fill(engine.bg_color)
 
-    for x in range(len(grid)):
-        row = grid[x]
-        for y in range(len(row)):
-            drawBlock(screen, grid[x][y])
-
+    particles.update(dt,screen)
 
     dt = clock.tick(engine.fps)/1000.0
     pygame.display.flip()
+
