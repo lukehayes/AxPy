aae1b01c9a2be1c1728e4160016e748b8868b8ea
lukehayes <lukepurplehayes@gmail.com>
2021-08-03 15:55:49 +0100

    Added docstring to lerp function.

--- core/math/utils.py
+++ core/math/utils.py
@@ -1,7 +1,19 @@
 #- Math Utility Functions  -------------------------------------------------#
 
 def lerp(value, start, end):
+    """
+    Linearly interpolate between two values.
+
+    Args:
+        value (int): The initial value
+        start (int): The starting value
+        end   (int): The ending value
+
+    Returns:
+        Float: Linear interpolation between start and end.
+    """
     return (value - start) * (
         1 / (end - start)
     )
+
 
