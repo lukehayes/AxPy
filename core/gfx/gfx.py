d2dfbae10b6cc7f7aec4ae6af3403a6354a0b60e
lukehayes <lukepurplehayes@gmail.com>
2021-08-03 18:24:41 +0100

    Added docstring to gfx draw* functions.

--- core/gfx/gfx.py
+++ core/gfx/gfx.py
@@ -1,15 +1,47 @@
 import pygame
 
 def draw(screen, x, y, w, h):
-    """Wrapper for pygame drawing"""
+    """
+    Simple drawing function that simplifies pygame's.
+
+    Args:
+        screen  (pygame.Surface): The initial value
+        x       (int)           : The x position
+        y       (int)           : The y position
+        w       (int)           : The width
+        h       (int)           : The height
+
+    Returns:
+        None: Draws a pygame Rect
+    """
     pygame.draw.rect(screen, pygame.Color(255,0,255), pygame.Rect(x,y,w,h))
 
+
 def drawRect(screen, rect):
-    """Draw a pygame.Rect abstraction"""
+    """
+    Draw a Rect
+
+    Args:
+        screen  (pygame.Surface): The initial value
+        rect    (pygame.Rect)   : The Rect to draw
+
+    Returns:
+        None: Draws a pygame Rect
+    """
     pygame.draw.rect(screen, pygame.Color(255,0,255), rect)
 
+
 def drawBlock(screen, block):
-    """Draw a block abstraction"""
+    """
+    Draw a block
+
+    Args:
+        screen  (pygame.Surface): The initial value
+        block   (core.block)   : The Block to draw
+
+    Returns:
+        None: Draws a Block
+    """
     pygame.draw.rect(screen,
                      pygame.Color(block.r, block.g,block.b),
                      pygame.Rect(block.x, block.y, block.width, block.height)
