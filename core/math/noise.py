8be856c13ae4df27156863d8f74351cbb9a49133
lukehayes <lukepurplehayes@gmail.com>
2021-08-03 16:03:57 +0100

    Added docstring to for Noise.noise2D function.

--- core/math/noise.py
+++ core/math/noise.py
@@ -1,9 +1,21 @@
 from opensimplex import OpenSimplex
+import random
 
 class Noise(object):
 
     @classmethod
-    def noise(cls, x,y):
+    def noise2D(cls, x,y):
+        """
+        Create 2D noise.
+
+        Args:
+            cls (object): The initial value
+            x   (int)   : The starting value
+            y   (int)   : The ending value
+
+        Returns:
+            Float: Simplex noise value for x and y.
+        """
         os = OpenSimplex()
         return os.noise2d(x,y)
 
