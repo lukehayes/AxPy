39cf9f26d5a9c546baee9ca1a5e6340d7c472e03
lukehayes <lukepurplehayes@gmail.com>
2021-08-03 16:10:03 +0100

    Added optional seed argument for Noise.noise2D().

--- core/math/noise.py
+++ core/math/noise.py
@@ -4,18 +4,19 @@ import random
 class Noise(object):
 
     @classmethod
-    def noise2D(cls, x,y):
+    def noise2D(cls, x,y, seed = 0):
         """
         Create 2D noise.
 
         Args:
-            cls (object): The initial value
-            x   (int)   : The starting value
-            y   (int)   : The ending value
+            cls  (object): The initial value
+            x    (int)   : The starting value
+            y    (int)   : The ending value
+            seed (int)   : The seed optional seed value for OpenSimplex
 
         Returns:
             Float: Simplex noise value for x and y.
         """
-        os = OpenSimplex()
+        os = OpenSimplex(seed)
         return os.noise2d(x,y)
 
