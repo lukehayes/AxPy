f5b3f48eeaf295945633067ea726838af34354c4
lukehayes <lukepurplehayes@gmail.com>
2021-08-05 10:25:25 +0100

    Added scale factor for x and y axes in Sprite class.

--- core/gfx/sprite.py
+++ core/gfx/sprite.py
@@ -2,7 +2,7 @@ import pygame
 
 class Sprite(object):
     """docstring for Sprite"""
-    def __init__(self, file, x = 0,y = 0):
+    def __init__(self, file, x = 0,y = 0, sx = 1, sy = 1):
         """
         Constructor
 
@@ -10,9 +10,12 @@ class Sprite(object):
             file  (str): The name of the image file
             x  (int): The x position of the sprite
             y  (int): The y position of the sprite
+            sx (int): The scale factor on x axis
+            sy (int): The scale factor on y axis
         """
         super().__init__()
         self.image = pygame.image.load("res/sprites/" + file).convert()
+        self.image = pygame.transform.scale(self.image, (int(1042/sx), int(768/sy) ))
         self.x = x
         self.y = y
 
