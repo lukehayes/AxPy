3d04deb52e2e15469ac4464248c7055062f13498
lukehayes <lukepurplehayes@gmail.com>
2021-08-08 19:43:28 +0100

    AnimatedSprite class re-written and now mostly working.

--- core/gfx/animated_sprite.py
+++ core/gfx/animated_sprite.py
@@ -2,28 +2,35 @@ from core.gfx.sprite import Sprite
 import pygame
 
 class AnimatedSprite(Sprite):
-    def __init__(self, file, x = 0,y = 0, sx = 1, sy = 1, frame_size = 16):
-        super().__init__(file, x, y, sx, sy)
-        self.frame_size = frame_size
+
+    def __init__(self, file, x = 0,y = 0, speed = 0.5):
+        super().__init__(file, x, y)
+
+        self.tile_size = 16
+        self.scale_factor = 6
         self.current_frame = 0
-        self.rect = pygame.Rect(0,0,self.frame_size, self.frame_size)
+        self.speed = speed
+        self.counter = 0
 
-    def play(self, screen, speed):
-        animation = self.image.subsurface(self.rect)
+        self.image = pygame.transform.scale(
+                        self.image,(
+                            self.image.get_width() * self.scale_factor,
+                            self.image.get_height() * self.scale_factor
+                        )
+                    )
 
-        # screen.blit(self.image, (self.x, self.y), pygame.Rect(0,8 * x, 8 * 5,8 * 5))
+    def update(self, dt, screen):
+        ts = self.tile_size
+        sf = self.scale_factor
+        speed = 0.5
+        max_frame = 4
 
-        self.current_frame += (1 * self.scale_x)
+        self.counter += dt
 
-        if self.current_frame > (1 * self.scale_x ):
-            self.current_frame = 0
+        if self.counter > self.speed:
+            self.current_frame += 1
 
+        print(int(self.counter))
 
-        screen.blit(self.image, (0,0), pygame.Rect(
-            (self.frame_size * self.scale_x) * 6 + (self.current_frame * self.scale_x),
-            (self.frame_size * self.scale_y) * 3,
-            self.frame_size * self.scale_x,
-            self.frame_size * self.scale_y)
-        )
-
+        screen.blit(self.image, (200,200), pygame.Rect((ts * sf) * int(self.counter), (ts * sf) * 3,ts * sf,ts * sf))
 
