c52334f1b79f9d84d963685a2c63940b5580371c
lukehayes <lukepurplehayes@gmail.com>
2021-08-09 09:47:47 +0100

    Animation is now working in AnimatedSprite.

--- core/gfx/animated_sprite.py
+++ core/gfx/animated_sprite.py
@@ -3,34 +3,32 @@ import pygame
 
 class AnimatedSprite(Sprite):
 
-    def __init__(self, file, x = 0,y = 0, speed = 0.5):
+    def __init__(self, file, x = 0,y = 0, speed = 0.1):
         super().__init__(file, x, y)
 
-        self.tile_size = 16
-        self.scale_factor = 6
         self.current_frame = 0
         self.speed = speed
         self.counter = 0
 
-        self.image = pygame.transform.scale(
-                        self.image,(
-                            self.image.get_width() * self.scale_factor,
-                            self.image.get_height() * self.scale_factor
-                        )
-                    )
-
     def update(self, dt, screen):
         ts = self.tile_size
         sf = self.scale_factor
-        speed = 0.5
-        max_frame = 4
+        max_frame = 5
 
         self.counter += dt
 
         if self.counter > self.speed:
             self.current_frame += 1
+            self.counter = 0
 
-        print(int(self.counter))
+            if self.current_frame > max_frame:
+                self.current_frame = 0
 
-        screen.blit(self.image, (200,200), pygame.Rect((ts * sf) * int(self.counter), (ts * sf) * 3,ts * sf,ts * sf))
+
+        screen.blit(self.image, (200,200), pygame.Rect(
+                                (ts * sf) * int(self.current_frame),
+                                (ts * sf) * 4,
+                                ts * sf,
+                                ts * sf)
+                    )
 
