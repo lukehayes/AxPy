6f6c2251d1260bd080d4d016fbfb755fe812da5c
lukehayes <lukepurplehayes@gmail.com>
2021-08-11 10:29:32 +0100

    Added core.gfx TileMeta class.

---
+++ core/gfx/tile_meta.py
@@ -0,0 +1,32 @@
+class TileMeta(object):
+    """
+    TileMeta objects hold all of the information than an AnimatedSprite needs to
+    run all of the different animations.
+    """
+    def __init__(self):
+        self.meta = {
+            'idle' : {
+                'start_frame' : 1,
+                'max_frames' : 6,
+            },
+            'running' : {
+                'start_frame' : 2,
+                'max_frames' : 6,
+            },
+        }
+
+    def has(self, meta_name):
+        """
+        Check if a specific meta name is available.
+
+        Args:
+            meta_name (str): The name of the meta to find.
+
+        Returns:
+            Bool
+        """
+        if meta_name in self.meta:
+            return True
+        else:
+            return False
+
