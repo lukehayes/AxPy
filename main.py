c192fac71a1ef00397f2d39936ef50be183555e0
lukehayes <lukepurplehayes@gmail.com>
2021-09-28 20:51:16 +0100

    Added escape key event that calls pygame.quit().

--- main.py
+++ main.py
@@ -24,20 +24,23 @@ running = True
 dt = 0
 clock  = pygame.time.Clock()
 
-s = AnimatedSprite("spritesheet.png")
+# s = AnimatedSprite("spritesheet.png")
+s = Sprite("spritesheet.png")
 s.x = 100
-
 
 while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                pygame.quit()
 
     screen.fill(engine.bg_color)
 
-    s.play('idle')
-    s.update(dt, screen)
+    screen.blit(s.image, (0,0)) # s.update(dt, screen)
 
     dt = clock.tick(engine.fps)/1000.0
     pygame.display.flip()
