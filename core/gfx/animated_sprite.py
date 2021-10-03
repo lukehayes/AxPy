d26638a7b488552b10d6e254576db645d62b2517
lukehayes <lukepurplehayes@gmail.com>
2021-10-03 11:02:39 +0100

    Moved duplicate functionality over to BaseSprite class.

--- core/gfx/animated_sprite.py
+++ core/gfx/animated_sprite.py
@@ -12,26 +12,14 @@ class AnimatedSprite(BaseSprite):
             x  (int): The x position of the sprite
             y  (int): The y position of the sprite
         """
-        super().__init__(x,y)
-        self.image = pygame.image.load("res/sprites/" + file).convert()
+        super().__init__(file, screen, x,y)
         self.xFrame = xFrame
         self.yFrame = yFrame
-        self.tile_size = 16
-        self.scale_factor = 6
         self.speed = 8
         self.animating = True
-        self.screen = screen
 
         self.cX = 4
         self.cY = 5
-
-        # Scale our image by a scale factor
-        self.image = pygame.transform.scale(
-            self.image,(
-                self.image.get_width() * self.scale_factor,
-                self.image.get_height() * self.scale_factor
-            )
-        )
 
     def update(self, dt):
 
@@ -45,16 +33,7 @@ class AnimatedSprite(BaseSprite):
             self.yFrame = int(self.cY)
 
     def draw(self, x = None, y = None):
-
-        # Check x and/or y are None here, use self version if so.
-        if x == None:
-            x = self.x
-
-        if y == None:
-            y = self.y
-
-        calc = self.tile_size * self.scale_factor
-        self.screen.blit(self.image, (x,y), (calc * self.xFrame, calc * self.yFrame, calc, calc))
+        BaseSprite.draw(self)
 
     @property
     def sx(self):
--- core/gfx/base_sprite.py
+++ core/gfx/base_sprite.py
@@ -1,7 +1,31 @@
+import pygame
 
 class BaseSprite(object):
 
-    def __init__(self, x = 10, y = 10):
+    def __init__(self, file, screen, x = 10, y = 10):
         self.x = x
         self.y = y
+        self.image = pygame.image.load("res/sprites/" + file).convert()
+        self.tile_size = 16
+        self.scale_factor = 6
+        self.screen = screen
+
+        # Scale our image by a scale factor
+        self.image = pygame.transform.scale(
+            self.image,(
+                self.image.get_width() * self.scale_factor,
+                self.image.get_height() * self.scale_factor
+            )
+        )
+
+    def draw(self, x = None, y = None):
+        # Check x and/or y are None here, use self version if so.
+        if x == None:
+            x = self.x
+
+        if y == None:
+            y = self.y
+
+        calc = self.tile_size * self.scale_factor
+        self.screen.blit(self.image, (x,y), (calc * self.xFrame, calc * self.yFrame, calc, calc))
 
