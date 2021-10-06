080aae8d29156d27ea3cfaf923e1e24ff3b9bd24
lukehayes <lukepurplehayes@gmail.com>
2021-10-06 21:33:44 +0100

    Added beginings of ComponentManager class.

---
+++ core/component/ComponentManager.py
@@ -0,0 +1,9 @@
+import pygame
+
+class ComponentManager(object):
+    """docstring for ComponentManager"""
+    def __init__(self):
+        self.positions = []
+        self.velocities = []
+        self.drawables = []
+
